e35433dae536dc2a39d0d8c3bed1f2e6ffc189bc
Ihor <zibi_man@ukr.net>
2024-04-07 14:22:38 +0200

    cycle_2

--- js/script.js
+++ js/script.js
@@ -30,3 +30,78 @@
 // 	number++;
 // }
 // out.innerHTML = str;
+//вывести числа 4 7 13 16 с помощью цикла
+// let out = document.getElementById("out");
+// let number = 4;
+// let str = "";
+// while(number <= 17) {
+// 	str +=number + " ";
+// 	number=number+3;
+// }
+// out.innerHTML = str;
+// Цикл for
+// let out = document.getElementById("out");
+// let str = "";
+// for (let i=4; i<=16; i=i+3){
+// 	str = str + i + " ";
+// }
+// out.innerHTML = str;
+// Вывести все годы с 1971 по 2024 год
+// Цикл while
+// let out = document.getElementById("out");
+// let number = 1971;
+// let str = "";
+// while(number <= 2024) {
+// 	str +=number + " ";
+// 	number++;
+// }
+// out.innerHTML = str;
+// Cycl for
+// let out = document.getElementById("out");
+// let str = "";
+// for (let i=1971; i<=2024; i++){
+// 	str = str + i + "<br>";
+// }
+// out.innerHTML = str;
+// вывести числа -4 -2 0 2 4 6 ....100
+// Цикл while
+// let out = document.getElementById("out");
+// let number = -4;
+// let str = "";
+// while(number <= 100) {
+// 	str +=number + "<br>";
+// 	number=number+2;
+// }
+// out.innerHTML = str;
+// Cycl for
+// let out = document.getElementById("out");
+// let str = "";
+// for (let i=-4; i<=100; i=i+2){
+// 	str = str + i + " ";
+// }
+// out.innerHTML = str;
+//С помощью цикла напечатайте таблицу умножения на 7 от 1 до 9.
+// Цикл while
+// let out = document.getElementById("out");
+// let number = 1;
+// let str = "";
+// while(number <= 9) {
+// 	str +=+7 + " * " +number+ " = " + 7 * number + "<br>";
+// 	number++;
+// }
+// out.innerHTML = str;
+// let out = document.getElementById("out");
+// let number = 1;
+// let str = "";
+// while(number <= 9) {
+// 	str +=7 * number + "<br>";
+// 	number++;
+// }
+// out.innerHTML = str;
+// Cycl for
+// let out = document.getElementById("out");
+// let str = "";
+// for (let i=1; i<=9; i=i+1){
+// 	str = str +7 + " * " +i+ " = " + 7 * i + "<br>";
+// }
+// out.innerHTML = str;
